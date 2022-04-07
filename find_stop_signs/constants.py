67e1fe1d4ab8111291a62109258aad97941cd295
clara <10088152+claraj@users.noreply.github.com>
2022-04-06 22:24:44 -0500

    tidying

--- find_stop_signs/constants.py
+++ find_stop_signs/constants.py
@@ -10,22 +10,17 @@ PADDING = bytearray(BLACK.to_bytes(1, 'big'))
 
 
 # Dividing image into chunks
-MIN_CHUNK_SIZE = 350  # chunks are squares, smallest square used if image is larger than this size in height or width 
-CHUNK_OVERLAP = 0.5  # how much chunks overlap as a percentage 0-1 
+MIN_CHUNK_SIZE = 500  # chunks are squares, smallest square used if image is larger than this size in height or width 
+CHUNK_OVERLAP = 0.3  # how much chunks overlap as a percentage 0-1.  0.90 is 10% overlap, 0.5 is 50% overlap, 0.3 is 70% overlap
 
 SOBEL_EDGE_THRESHOLD = 120  # larger value, to get sharp edges for stop signs, still finds edges with lower number but more noise
 
 # Hough transform options 
-HOUGH_INTERESTING_LINE_THRESHOLD = 0.60  # as a percent of all the lines
+HOUGH_INTERESTING_LINE_THRESHOLD = 0.70  # as a percent of all the lines
 THETA_STEP = 1
 D_STEP = 1
 
 # Finding lines 
-VARIATION_FROM_45 = 10   # how far away from 135 do lines need to be to be considered an octagon?  
-MIN_LINES_FOR_STOP_SIGN = 6  # not that hard mode 
+VARIATION_FROM_45 = 5   # how far away from 135 do lines need to be to be considered an octagon?  
+MIN_LINES_FOR_STOP_SIGN = 7  # in case an edge is obscured
 INTERCEPT_SEPARATION = 30  # how far apart should lines be? This is not very flexible TODO revisit
-
-
-
-
-
--- find_stop_signs/stop_sign_finder.py
+++ find_stop_signs/stop_sign_finder.py
@@ -11,20 +11,25 @@ from pixel_iterator import block_of_pixels_iterator, convert_line_to_array, imag
 from constants import *
 
 
-filename = 'bw_images/image-4.bmp'
+filename = 'bw_images/image-9.bmp'
 
-# pre_thinned = 'out/bw_images/pre_thinned/image-4-stop_thinned.bmp'
-# filename = pre_thinned
+"""
+pre_thinned = 'out/bw_images/pre_thinned/image-9_thinned.bmp'
+filename = pre_thinned
+"""
 
 @timer
 def find_stop_signs():
 
+    """
     # used when working with pre-saved thinned images 
-    # header, color_table, thinned_array = read_bmp_file(pre_thinned)
-    # print('Get dimensions')
-    # height, width, row_length = dimensions(header)
-    # is_there_a_stop_sign(thinned_array, height, width, row_length, chunk_image=True)
-    # exit()
+    header, color_table, thinned_array = read_bmp_file(pre_thinned)
+    print('Get dimensions')
+    height, width, row_length = dimensions(header)
+    is_there_a_stop_sign(thinned_array, height, width, row_length, chunk_image=True)
+    exit()
+    """
+
 
     print('Read bitmap')
     header, color_table, original_file_array = read_bmp_file(filename)
@@ -52,18 +57,16 @@ def find_stop_signs():
 
 def is_there_a_stop_sign(image_bytes, height, width, row_length, chunk_image=False):
 
-    # print('examining entire image')
+    print('examining entire image')
 
-    # lines, bounding_box = find_stop(image_bytes, height, width, row_length)   # bounding_box needs to be a list with coords [top left x, top left y, bottom right x, bottom right y]
-    # if bounding_box:
-    #     # im_with_square.rectangle([start_col, start_row, start_col+chunk_width, start_row+chunk_height], outline=128)
-    #     return    
+    lines, bounding_box = find_stop(image_bytes, height, width, row_length, 0, 0)   # bounding_box needs to be a list with coords [top left x, top left y, bottom right x, bottom right y]
+    if bounding_box:  # if stop sign found,
+        return    
 
     if chunk_image:
 
         print('Examining blocks')
 
-        # what size chunks? 
         chunk_size = int(min(height, width, MIN_CHUNK_SIZE))
         chunk_overlap = int(chunk_size * CHUNK_OVERLAP)
 
@@ -73,17 +76,17 @@ def is_there_a_stop_sign(image_bytes, height, width, row_length, chunk_image=Fal
         for index, chunk in enumerate(image_chunk_iterator(image_bytes, height, width, row_length, chunk_size, chunk_overlap)):
 
             pixel_chunk, start_row, start_col, chunk_height, chunk_width = chunk    
-            lines, bounding_box = find_stop(pixel_chunk, chunk_height, chunk_width, chunk_width)
+            lines, bounding_box = find_stop(pixel_chunk, chunk_height, chunk_width, chunk_width, start_col, start_row)
 
             im_with_square.rectangle([start_col, start_row, start_col+chunk_width, start_row + chunk_height], outline=0) 
 
-            if bounding_box:                
-                break    # if we only expect 1 stop sign. Or could carry on and look for others?
+            # if bounding_box:                
+            #     break    # if we only expect 1 stop sign. Or could carry on and look for others?
             
         im.show()
 
 
-def find_stop(image_bytes, height, width, row_length):  
+def find_stop(image_bytes, height, width, row_length, offset_width, offset_height):  
 
     lines, pixels_for_lines = hough_transform.find_lines(image_bytes, height, width, row_length) 
 
@@ -92,35 +95,31 @@ def find_stop(image_bytes, height, width, row_length):
     result = stop_sign.find_stop_sign(lines, pixels_for_lines, max_d)
 
     if result:
-
         im = Image.open(filename)
         im_with_square = ImageDraw.Draw(im)
 
-        stop_sign_lines, bounding_box = result #= stop_sign.find_stop_sign(lines, pixels_for_lines, max_d)
+        stop_sign_lines, bounding_box = result
+
+        bounding_box[0] += offset_width
+        bounding_box[2] += offset_width
+        
+        bounding_box[1] += offset_height
+        bounding_box[3] += offset_height
 
         im_with_square.rectangle(bounding_box, outline=0) 
         im.show() 
        
-    # if stop_sign_lines:
-        # print('there seems to be a stop sign in the square at ', start_row, start_col, chunk_height, chunk_width)
-        print('there seems to be a stop sign in the square given by lines')
+        print('There seems to be a stop sign in the square given by lines')
         for l in stop_sign_lines:
             print(l)
 
         hough_transform.plot_lines(stop_sign_lines, height, width, max_d)
 
-        # stop_sign_pixels, bounding_box = stop_sign.stop_sign_location_for_lines(lines)
-        # bounding_box = stop_sign.bounding_box_for_pixels(stop_sign_pixels)
         return stop_sign_lines, bounding_box 
 
     else:
         print('No stop sign found')
         return None, None 
-
-
-
-
-
 
 
 if __name__ == '__main__':
